39b3b270b8791cd0ccd02a9ab87228ad567bc98e
Arye <dabergshoeff@gmail.com>
2020-09-22 14:52:56 +0200

    First version of flying

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform Target;
+    public float RotateSpeed = 2f;
+    public float MoveSpeed = 2f;
+    public float Distance = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotating towards Target
+        Vector3 lookHeading = Target.position - transform.position;
+        Quaternion rot = Quaternion.LookRotation(lookHeading);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, RotateSpeed * Time.deltaTime);
+
+        //Moving towards target
+        Vector3 followPosition = Target.position - Target.forward * Distance;
+        Vector3 followHeading = followPosition - transform.position;
+        float dist = followHeading.magnitude;
+        Vector3 followDir = followHeading / dist;
+
+        dist = Mathf.Clamp(dist, 0f, 1f);
+
+        transform.position = transform.position + followDir * Time.deltaTime * MoveSpeed * dist;
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,25 +7,25 @@ public class PlayerController : MonoBehaviour
 {
     public float MoveSpeed = 3f;
     public float RotateSpeed = 3f;
+    public float MaxRotation = 60f;
 
     private Vector2 targetDirection;
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = transform.position + transform.forward * Time.deltaTime * MoveSpeed;
+
         if (targetDirection.sqrMagnitude < 0.01f)
             return;
 
-        transform.position = transform.position + (Camera.main.transform.forward * targetDirection.y + Camera.main.transform.right * targetDirection.x) * Time.deltaTime * MoveSpeed;
+        transform.Rotate(0f, targetDirection.x * RotateSpeed, 0f);
+        transform.Rotate(targetDirection.y * RotateSpeed, 0f, 0f);
 
-        float f = transform.rotation.eulerAngles.z + targetDirection.x;
-        if (f >= 360)
-            f -= 360;
-        else if (f <= -360)
-            f += 360;
-        Debug.Log(f);
-        if (f < 40f && f > -40f)
-            transform.Rotate(0f, 0f, targetDirection.x);
+
+        transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0f));
+
+        //transform.Rotate(0f, targetDirection.x, 0f);
         /*
         Quaternion targetDir = Quaternion.LookRotation(Camera.main.transform.forward * targetDirection.y + Camera.main.transform.right * targetDirection.x);
         Quaternion rot = Quaternion.Slerp(transform.rotation, targetDir, 0.01f);
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
     }
 
     public void OnRightStick(InputValue value) {
-        FreeLookCam.Instance.RightStickValues = value.Get<Vector2>();
+        //FreeLookCam.Instance.RightStickValues = value.Get<Vector2>();
     }
 
     private void OnMove(InputValue context) {
