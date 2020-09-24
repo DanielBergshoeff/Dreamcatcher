f62234b2b3b30d630f1d59a72ad939c81ef99157
Arye <dabergshoeff@gmail.com>
2020-09-24 16:37:02 +0200

    Added triangle formation

---
+++ Assets/Scripts/Pillar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PillarManager.Instance.AllPillars.Add(this);
+    }
+}
