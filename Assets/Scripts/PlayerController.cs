2eb26007e4b495cee1f7287c7db7e40bf1213d90
Arye <dabergshoeff@gmail.com>
2020-09-28 14:59:31 +0200

    Implemented Jantiene feedback (rough)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     public float MoveSpeed = 3f;
     public float RotateSpeed = 3f;
     public float MaxRotation = 60f;
-
-    public float DownwardsSpeedBonus = 3f;
-    public float MinDownwardsAngle = 80f;
-
     public float BindRotationSpeed = 120f;
 
+    [Header("Misc")]
+    public float DownwardsSpeedBonus = 3f;
+    public float MinDownwardsAngle = 80f;
     public GameObject TriangleCanvas;
+
+    [Header("Collision aversion")]
+    public float CheckDistance = 5f;
+    public LayerMask CollisionLayer;
 
     private float bonusSpeed = 0f;
     private float flapSpeed = 0f;
@@ -30,12 +34,44 @@ public class PlayerController : MonoBehaviour
     private Vector3 pillarPoint;
     private float currentRot = 0f;
     private float direction = 0f;
-    private bool leftAnchor = false;
-    private Vector3 startPosition;
+
+    private float aversionStrength = 0f;
+    private float aversionDirection = 0f;
+
+    private bool rotateAroundSelf = false;
 
     private void Awake() {
         body = transform.GetChild(0);
-        startPosition = transform.position + transform.right * 1f;
+    }
+
+    private void OnLeftShoulder() {
+        if (!rotateAroundSelf) {
+            direction = -1;
+            currentRot = 0f;
+            rotateAroundSelf = true;
+        }
+        else {
+            rotateAroundSelf = false;
+        }
+    }
+
+    private void OnRightShoulder() {
+        if (!rotateAroundSelf) {
+            direction = 1;
+            currentRot = 0f;
+            rotateAroundSelf = true;
+        }
+        else {
+            rotateAroundSelf = false;
+        }
+    }
+
+    public void OnR2() {
+        FreeLookCam.Instance.OnR2();
+    }
+
+    public void OnL2() {
+        FreeLookCam.Instance.OnL2();
     }
 
     private void OnBind() {
@@ -54,49 +90,89 @@ public class PlayerController : MonoBehaviour
         direction = angleRight > angleLeft ? -1f : 1f;
 
         inPillar = false;
+
+        FreeLookCam.Instance.RotatingAroundPillar = true;
     }
 
     // Update is called once per frame
     void Update() {
-        /*
-        if (!leftAnchor) {
-            transform.RotateAround(transform.position + startPosition, Vector3.up, BindRotationSpeed * direction * Time.deltaTime);
-            return;
-        }*/
-
         if (aroundPillar) {
-            transform.RotateAround(pillarPoint, Vector3.up, BindRotationSpeed * direction * Time.deltaTime);
-            currentRot += BindRotationSpeed * Time.deltaTime;
-            if (currentRot >= 360f) {
-                aroundPillar = false;
-                PillarManager.Instance.AddPillarToBind(currentPillar, pillarPoint);
-            }
+            RotateAroundPillar();
             return;
         }
 
+        ApplyMovement();
 
-        if (bonusSpeed > 0f) {
-            bonusSpeed -= Time.deltaTime;
-            FollowCamera.Instance.BonusMoveSpeed = (bonusSpeed / 5) * 4;
+        if (rotateAroundSelf) {
+            RotateAroundSelf();
+            return;
         }
 
-        if (flapSpeed > 0f)
-            flapSpeed -= Time.deltaTime;
+        CollisionAversion();
+        ApplyRotation();
+    }
 
+    private void RotateAroundSelf() {
+        transform.RotateAround(transform.position + transform.right * direction, Vector3.up, 1f * direction);
+        currentRot += 1f;
+    }
+
+    private void CollisionAversion() {
+        //Forward check
+        RaycastHit hit;
+        if (!Physics.SphereCast(transform.position, 0.5f, transform.forward, out hit, CheckDistance, CollisionLayer, QueryTriggerInteraction.Ignore)) {
+            aversionStrength = 0f;
+            return;
+        }
+
+        aversionStrength = 1f - hit.distance / CheckDistance;
+
+        for (float i = 0.1f; i <= 1f; i += 0.1f) {
+            if (!Physics.SphereCast(transform.position, 0.5f, transform.forward * (1f - i) - transform.right * i, out hit, CheckDistance, CollisionLayer, QueryTriggerInteraction.Ignore)) {
+                aversionDirection = i * -1f;
+                break;
+            }
+            if (!Physics.SphereCast(transform.position, 0.5f, transform.forward * (1f - i) + transform.right * i, out hit, CheckDistance, CollisionLayer, QueryTriggerInteraction.Ignore)) {
+                aversionDirection = i;
+                break;
+            }
+        }
+
+        aversionDirection = 1f;
+    }
+
+    private void ApplyRotation() {
         if (wingPosition > 1f * targetDirection.x && targetDirection.x < 0f)
             wingPosition += targetDirection.x * Time.deltaTime * 3f;
-        else if (wingPosition < 1f  * targetDirection.x && targetDirection.x > 0f)
+        else if (wingPosition < 1f * targetDirection.x && targetDirection.x > 0f)
             wingPosition += targetDirection.x * Time.deltaTime * 3f;
         else if (wingPosition > 0.03f && targetDirection.x <= 0.01f)
             wingPosition -= Time.deltaTime * 3f;
         else if (wingPosition < -0.03f && targetDirection.x >= -0.01f)
             wingPosition += Time.deltaTime * 3f;
-        else if(Mathf.Abs(targetDirection.x) < 0.01f){
+        else if (Mathf.Abs(targetDirection.x) < 0.01f) {
             wingPosition = 0f;
         }
 
-
         body.rotation = Quaternion.Euler(new Vector3(body.rotation.eulerAngles.x, body.rotation.eulerAngles.y, wingPosition * -60f));
+        transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0f));
+
+        if (targetDirection.sqrMagnitude < 0.01f && aversionStrength < 0.01f)
+            return;
+
+        float rotationHorizontal = targetDirection.x * (1f - aversionStrength) + aversionDirection * aversionStrength * 3f;
+        transform.Rotate(0f, rotationHorizontal * RotateSpeed, 0f);
+        transform.Rotate(targetDirection.y * RotateSpeed, 0f, 0f);
+    }
+
+    private void ApplyMovement() {
+        if (bonusSpeed > 0f) {
+            bonusSpeed -= Time.deltaTime;
+            FreeLookCam.Instance.BonusMoveSpeed = (bonusSpeed / 5) * 4;
+        }
+
+        if (flapSpeed > 0f)
+            flapSpeed -= Time.deltaTime;
 
         float angle = Vector3.Angle(-Vector3.up, transform.forward);
         float downward = 1f - Mathf.Clamp(angle, 0f, MinDownwardsAngle) / MinDownwardsAngle;
@@ -104,14 +180,16 @@ public class PlayerController : MonoBehaviour
 
         float speed = MoveSpeed + bonusSpeed + flapSpeed;
         transform.position = transform.position + transform.forward * Time.deltaTime * speed;
+    }
 
-        transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0f));
-
-        if (targetDirection.sqrMagnitude < 0.01f)
-            return;
-
-        transform.Rotate(0f, targetDirection.x * RotateSpeed, 0f);
-        transform.Rotate(targetDirection.y * RotateSpeed, 0f, 0f);
+    private void RotateAroundPillar() {
+        transform.RotateAround(pillarPoint, Vector3.up, BindRotationSpeed * direction * Time.deltaTime);
+        currentRot += BindRotationSpeed * Time.deltaTime;
+        if (currentRot >= 360f) {
+            aroundPillar = false;
+            PillarManager.Instance.AddPillarToBind(currentPillar, pillarPoint);
+            FreeLookCam.Instance.RotatingAroundPillar = false;
+        }
     }
 
     private void OnJump() {
@@ -119,7 +197,7 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnRightStick(InputValue value) {
-        //FreeLookCam.Instance.RightStickValues = value.Get<Vector2>();
+        FreeLookCam.Instance.RightStickValues = value.Get<Vector2>();
     }
 
     private void OnMove(InputValue context) {
