413be6dd78e8ac78ea33c164e595a7d3b7b10144
Arye <dabergshoeff@gmail.com>
2020-10-20 16:42:25 +0200

    Added pause menu functionality

---
+++ Assets/Scripts/AppHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+public static class AppHelper
+{
+#if UNITY_WEBPLAYER
+     public static string webplayerQuitURL = "https://fairfire.itch.io/threads-of-the-past";
+#endif
+    public static void Quit() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+#else
+         Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PausePanel;
+
+    private bool paused = false;
+
+
+    private void Awake() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (!paused)
+                Pause();
+            else
+                UnPause();
+        }
+    }
+
+    public void Quit() {
+        AppHelper.Quit();
+    }
+
+    public void Pause() {
+        Time.timeScale = 0f;
+        PausePanel.SetActive(true);
+        paused = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void UnPause() {
+        Time.timeScale = 1f;
+        PausePanel.SetActive(false);
+        paused = false;
+
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
